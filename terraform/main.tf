087b79b9ff53d21af9e8f474f56be1eed71d1199
Pranitshriwas <pranitshrivas142001@gmail.com>
2025-06-25 15:53:36 +0530

    Add terraform workflow

---
+++ terraform/main.tf
@@ -0,0 +1,82 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+
+  tags = {
+    Name = "test vpc" # this is for vpc
+  }
+
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_security_group" "sg_train" {
+  name        = "sg_train"
+
+  vpc_id      = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "sg_train"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.sg_train.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.sg_train.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443       #Ingress Rules (Inbound Traffic)
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_train.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.sg_train.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"    # Egress Rules (Outbound Traffic)
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0b5ab71f6a75e8bae"
+  instance_type          = "m5.xlarge"
+  key_name               = "mlops"   # Yahan key pair ka naam
+
+  tags = {
+    Name = "my Ec2"
+  }
+}
