f4761acd8f9db7fed074f111886596eca8cfeb9f
Pranitshriwas <pranitshrivas142001@gmail.com>
2025-06-27 13:40:25 +0530

    some changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,8 @@ resource "aws_instance" "train_server" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/.ssh/mlops.pem")
+    private_key = file("/home/neosoft/.ssh/mlops.pem")
     host        = self.public_ip
+    timeout     = "5m"
   }
 }
